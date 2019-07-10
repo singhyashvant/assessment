94aa629a5c0b40a520a3956e5ced45e99c9d1602
Yashvant Singh <singh.yashvant@ymail.com>
2019-07-10 09:07:32 +0530

    CHANGED THE CHROME DRIVER PATH FROM ABSOLUTE TO RELATIVE

--- src/test/java/com/emirates/assess/DriverProvider.java
+++ src/test/java/com/emirates/assess/DriverProvider.java
@@ -13,10 +13,11 @@ public class DriverProvider {
 		return driver;
 	}
 	public WebDriver chromeDriver()
-	{
-	System.setProperty("webdriver.chrome.driver" , "/Users/yassingh7/eclipse-workspace/new_eclipse_workspace/assess/chromedriver");
+	{	
+	System.setProperty("webdriver.chrome.driver" , "./chromedriver");
 	driver= new ChromeDriver();
 	return driver;
 }
+	
 
 }
