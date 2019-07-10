b694380864123e5526eef4efbde00b00655700b8
Yashvant Singh <singh.yashvant@ymail.com>
2019-07-10 10:42:00 +0530

    ADDDED THE COMMON SUPPORT LIBRARY AND METHODS

--- src/test/java/com/emirates/assess/CommonLibrary.java
+++ src/test/java/com/emirates/assess/CommonLibrary.java
@@ -1,5 +1,7 @@
 package com.emirates.assess;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -8,12 +10,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonLibrary {
+	List<WebElement> elements = null;
 	WebElement element = null;
 	WebDriverWait wait = null;
 
 	public WebElement findElementByID(WebDriver driver,String locatorValue) {
 		try {
-			waitForElementToClickable(driver,By.id(locatorValue));
+			waitForElementToVisible(driver,By.id(locatorValue));
 			element = driver.findElement(By.id(locatorValue));
 			
 		} catch (NoSuchElementException e) {
@@ -22,7 +25,7 @@ public class CommonLibrary {
 	}
 	public WebElement findElementByName(WebDriver driver,String locatorValue) {
 		try {
-			waitForElementToClickable(driver,By.name(locatorValue));
+			waitForElementToVisible(driver,By.name(locatorValue));
 			element = driver.findElement(By.name(locatorValue));
 			
 		} catch (NoSuchElementException e) {
@@ -31,12 +34,21 @@ public class CommonLibrary {
 	}
 	public WebElement findElementByXpath(WebDriver driver, String locatorValue) {
 		try {
-			waitForElementToClickable(driver,By.xpath(locatorValue));
+			waitForElementToVisible(driver,By.xpath(locatorValue));
 			element = driver.findElement(By.xpath(locatorValue));
 			
 		} catch (NoSuchElementException e) {
 		}
 		return element;
+	}
+	public List<WebElement> findElementsByXpath(WebDriver driver, String locatorValue) {
+		try {
+			waitForElementToVisible(driver,By.xpath(locatorValue));
+			elements = driver.findElements(By.xpath(locatorValue));
+			
+		} catch (NoSuchElementException e) {
+		}
+		return elements;
 	}
 	public void waitForElementToClickable(WebDriver driver, By e)
 	{
@@ -46,9 +58,10 @@ public class CommonLibrary {
 	}
 	public void waitForElementToVisible(WebDriver driver, By e)
 	{
-		wait = new WebDriverWait(driver, 20);
+		wait = new WebDriverWait(driver, 60);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(e));
 		
 	}
+	
 
 }
