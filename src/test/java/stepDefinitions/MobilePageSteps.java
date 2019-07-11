c723572fe715695b013756e77e62013e0c0fab36
Yashvant Singh <singh.yashvant@ymail.com>
2019-07-11 15:50:09 +0530

    MOBILE STEPS COMPLETED

--- src/test/java/stepDefinitions/MobilePageSteps.java
+++ src/test/java/stepDefinitions/MobilePageSteps.java
@@ -10,7 +10,7 @@ public class MobilePageSteps extends MobilePage{
 
 @Given("^user has launch the emirates app$")
 public void user_has_launch_the_emirates_app() throws Throwable {
-	loginEmiratesApp("user","Pass");
+	isLaunchSuccess();
 }
 
 @Given("^user is logged in as a guest$")
@@ -21,20 +21,17 @@ public void user_is_logged_in_as_a_guest() throws Throwable {
 
 @Given("^user navigate to flight status screen$")
 public void user_navigate_to_flight_status_screen() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
+	tapStatusBottomNav();
 }
 
-@When("^user enters the abc to see the status$")
-public void user_enters_the_abc_to_see_the_status() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
+@When("^user enters the (.*) to see the status$")
+public void user_enters_the_abc_to_see_the_status(String flightNumber) throws Throwable {
+enterFlightNumber(flightNumber);
 }
 
 @Then("^user should be displayed with the listing$")
 public void user_should_be_displayed_with_the_listing() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
+isFlightStatusDisplayed();
 }
 
 
