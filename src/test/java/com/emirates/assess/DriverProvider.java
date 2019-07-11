66bd9cdbe74d4feb0b653fc0a2a378683f10a703
Yashvant Singh <singh.yashvant@ymail.com>
2019-07-11 09:08:00 +0530

    IMPLEMENTED THE ANDROID DRIVER FOR APPIUM

--- src/test/java/com/emirates/assess/DriverProvider.java
+++ src/test/java/com/emirates/assess/DriverProvider.java
@@ -1,12 +1,22 @@
 package com.emirates.assess;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 
 public class DriverProvider {
 	WebDriver driver = null;
-	WebDriver mobileDriver = null;
+	WebDriver mDriver = null;
+	WebDriverWait waitVar =null;
 	public WebDriver fireFoxDriver()
 	{
 		driver = new FirefoxDriver();
@@ -18,6 +28,27 @@ public class DriverProvider {
 	driver= new ChromeDriver();
 	return driver;
 }
-	
+	public void androidDriver() throws MalformedURLException, InterruptedException {
 
+        // set up appium
+        final File classpathRoot = new File(System.getProperty("user.dir"));
+        final File appDir = new File(classpathRoot, "src/test/resources/apps");
+        final File app = new File(appDir, "emirates.apk");
+
+        final DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("platformVersion", "9");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        mDriver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"),
+                capabilities);
+        waitVar = new WebDriverWait(mDriver, 90);
+
+        //Thread.sleep(90000);
+
+    }
+	 public void mobileTearDown(){
+         //close the app
+		 mDriver.quit();
+     }
 }
