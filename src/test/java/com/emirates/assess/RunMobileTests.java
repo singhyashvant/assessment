57ed4ecce87b642b438a6ca0ccf6ea9d718cbef3
Yashvant Singh <singh.yashvant@ymail.com>
2019-07-11 15:47:33 +0530

    TEST RUNNER FOR MOBILE

---
+++ src/test/java/com/emirates/assess/RunMobileTests.java
@@ -0,0 +1,21 @@
+package com.emirates.assess;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = {
+			 "pretty", "html:target/cucumberHtmlReport",
+		     "html:target/cucumberHtmlReport",     //  for html result
+			 "pretty:target/cucumber-json-report.json"   // for json result
+		     }
+		,features = "src/test/resources"
+		,glue = {""}   // predefined step definitions package
+		,tags= {"@flightStatus"}
+		,dryRun=false
+		,monochrome = true		)
+
+public class RunMobileTests { 	
+}
