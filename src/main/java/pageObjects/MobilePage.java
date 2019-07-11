77c22415fdfa44dc8da42a6202d94799ff5d20ab
Yashvant Singh <singh.yashvant@ymail.com>
2019-07-11 15:52:29 +0530

    REQUIRED PAGE METHODS COMPLETED

--- src/main/java/pageObjects/MobilePage.java
+++ src/main/java/pageObjects/MobilePage.java
@@ -5,16 +5,38 @@ import org.openqa.selenium.WebDriver;
 import com.emirates.assess.CommonLibrary;
 import com.emirates.assess.DriverProvider;
 
-public class MobilePage{
+public class MobilePage {
 	CommonLibrary locate = null;
+
 	public MobilePage() {
 		locate = new CommonLibrary();
 	}
-	DriverProvider driver = new  DriverProvider();
+
+	DriverProvider driver = new DriverProvider();
 	WebDriver ad = driver.androidDriver();
-	public void loginEmiratesApp(String usr,String pwd)
-	{
-		locate.findElementByID(ad, "login_button_login").click();
+
+	public void isLaunchSuccess() {
+		if (locate.findElementByID(ad, "bottom_navigation_flight_status").isDisplayed())
+		{
+			System.out.println("App launched successfully");
+		}
+
+	}
+	public void tapStatusBottomNav() {
+		locate.findElementByID(ad, "bottom_navigation_flight_status").click();
+		System.out.println("Tapped on status");
 		
-}
+
+	}
+	public void enterFlightNumber(String flightNumber)
+	{
+		locate.findElementByID(ad, "flight_number").sendKeys(flightNumber);
+		locate.findElementByID(ad, "flight_status_submit").click();
+		System.out.println("flight number entered");
+	}
+	public void isFlightStatusDisplayed()
+	{
+		locate.findElementByID(ad, "search_result_row_flight_no").isDisplayed();
+		System.out.println("flight status displayed successfully");
+	}
 }
