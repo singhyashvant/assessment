517bcac8917b9b2dc3def12aef9e5aa5d70529d6
Yashvant Singh <singh.yashvant@ymail.com>
2019-07-11 09:10:37 +0530

    ADDING THE MOBILE PAGES STEP DEFINITION WITH PENDING

---
+++ src/test/java/stepDefinitions/MobilePageSteps.java
@@ -0,0 +1,42 @@
+package stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MobilePageSteps {
+
+@Given("^user has launch the emirates app$")
+public void user_has_launch_the_emirates_app() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Given("^user is logged in as a guest$")
+public void user_is_logged_in_as_a_guest() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Given("^user navigate to flight status screen$")
+public void user_navigate_to_flight_status_screen() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^user enters the abc to see the status$")
+public void user_enters_the_abc_to_see_the_status() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^user should be displayed with the listing$")
+public void user_should_be_displayed_with_the_listing() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+
+
+}
